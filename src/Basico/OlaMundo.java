b7bc723062b442f7266c7b6a5e99576e456ed9c3
AssisFilhoNazario <assisfilhopfic@gmail.com>
2018-05-25 07:37:58 -0300

    Java Basico
    Conceitos de Java

---
+++ src/Basico/OlaMundo.java
@@ -0,0 +1,7 @@
+package Basico;
+
+public class OlaMundo {
+    public static void main (String [] args){
+        System.out.print("Ola Mundo");
+    }
+}
---
+++ src/Basico/TiposPrimitivos.java
@@ -0,0 +1,18 @@
+package Basico;
+
+public class TiposPrimitivos {
+    public static void main (String[]args){
+        int idade = 10;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numreroGrande = 1000L;
+        char caractere = 'A';
+        String nome = "Assis";
+
+    }
+
+}
