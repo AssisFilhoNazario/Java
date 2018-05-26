5879142462fc31a35cea4c835280d096ea67aeaf
AssisFilhoNazario <assisfilhopfic@gmail.com>
2018-05-25 21:43:27 -0300

    Condições
    Estrutura de condições concluidas

---
+++ src/CondicoesSimplesComposta/OperadorTernario.java
@@ -0,0 +1,15 @@
+package CondicoesSimplesComposta;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int idade = 20;
+        String status;
+//        if (idade < 18){
+//            status = "Não Adulto";
+//        }else{
+//            status = "Adulto";
+//        }
+        status = idade < 18 ? "Nao adulto" : "Adulto";
+        System.out.println(status);
+    }
+}
---
+++ src/SwitchExercicios/ExercicioSwitch.java
@@ -0,0 +1,32 @@
+package SwitchExercicios;
+
+public class ExercicioSwitch {
+    public static void main(String[] args) {
+        int dia = 18;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo é final de semana");
+                break;
+            case 2:
+                System.out.println("Segunda é dia util");
+                break;
+            case 3:
+                System.out.println("Terça é dia util");
+                break;
+            case 4:
+                System.out.println("Quarta é dia util");
+                break;
+            case 5:
+                System.out.println("Quinta é dia util");
+                break;
+            case 6:
+                System.out.println("Sexta aé dia util");
+                break;
+            case 7:
+                System.out.println("Sabado é final de semana");
+                break;
+            default:
+                System.out.println("Dia invalido");
+        }
+    }
+}
