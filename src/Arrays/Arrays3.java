2fbc3b382e0226e927b843b2c30794a85e75153b
AssisFilhoNazario <assisfilhopfic@gmail.com>
2018-05-27 22:59:06 -0300

    Arrays e fluxo de repeticao

---
+++ src/Arrays/Arrays3.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class Arrays3 {
+    public static void main(String[] args) {
+        int [] numeros = new int[5];
+        int [] numero2 = {2,6,9,8,10};
+
+//        for (int i = 0; i<numero2.length ; i++){
+//            System.out.println(numero2[i]);
+//        }
+
+        for (int aux : numero2){
+            System.out.println(aux);
+        }
+
+
+
+    }
+}
